4465911bb94a62978bfae8228ec368522f7a27c9
Harvs04 <hamartinez@up.edu.ph>
2023-03-14 16:52:13 +0800

    update

--- server.js
+++ server.js
@@ -8,11 +8,47 @@ app.listen(port, () => {
     console.log("Server is listening at port " + port);
 });
 
-app.get('/', (req, res) => {
-    res.send("Si idol pala to eh")
+app.get('/groceries', (req, res) => {
+    res.send([
+        {
+            name: "Pancit Canton",
+            flavor: "Extra Spicy",
+            quantity: 2,
+            price: 84
+        },
+        {
+            name: "Egg",
+            quantity: 12,
+            price: 112
+        },
+        {
+            name: "Chicken",
+            quantity: 1,
+            price: 270
+        }
+    ]);
 })
 
-app.post('/submit-data', (req, res) => {
-    res.send("POSTED!");
+app.get('/about', (req, res) => {
+    res.send("Welcome to our about page.");
 })
+
+app.get('/', (req, res) => {
+    res.send(
+        `<h1>Enter your age: </h1>
+        <form action="/result" method="POST">
+            <input type="number">
+            <button>Submit</button>
+        </form>
+        `
+    );
+})
+
+app.post('/result', (req, res) => {
+    res.send("Thank you for answering the form");
+})
+
+// app.post('/submit-data', (req, res) => {
+//     res.send("POSTED!");
+// })
 
